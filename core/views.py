40181f69c7eaf3533d4cb23b8f3ca9de5d071a8b
Renan Garcia <renangarciasoz@hotmail.com>
2017-05-10 00:38:06 -0300

    Arquivos para deploy Heroku

--- core/views.py
+++ core/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'pages/index.html')
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'pages/contact.html')
 
 
 def product_list(request):
-    return render(request, 'product_list.html')
+    return render(request, 'pages/product_list.html')
 
 
 def product(request):
-    return render(request, 'product.html')
+    return render(request, 'pages/product.html')
