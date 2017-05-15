20ff4dc839e6b50fe3bf912223a6f7d209924bfd
Renan Garcia <renangarciasoz@hotmail.com>
2017-05-15 20:17:34 -0300

    Criar login e permitir usuário fazer cadastro

--- core/views.py
+++ core/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.conf import settings
-from catalog.models import Category
 from django.core.mail import send_mail
-from django.views.generic import View, TemplateView
+from django.conf import settings
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import View, TemplateView, CreateView
+from django.contrib.auth import get_user_model
 
 from .forms import ContactForm
 
+
+
+User = get_user_model()
 
 class IndexView(TemplateView):
     template_name = 'pages/index.html'
@@ -26,3 +31,14 @@ def contact(request):
         'sucess': sucess
     }
     return render(request, 'pages/contact.html', context)
+
+
+class RegisterView(CreateView):
+
+    form_class = UserCreationForm
+    template_name = 'pages/register.html'
+    model = User
+    success_url = reverse_lazy('index')
+
+
+register = RegisterView.as_view()
