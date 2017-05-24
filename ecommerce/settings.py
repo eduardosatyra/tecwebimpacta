907bacf9c14059017799df8cec76a39b10c26ef4
Renan Garcia <renangarciasoz@hotmail.com>
2017-05-24 00:49:35 -0300

    Adicionar checkout

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'core',
     'accounts',
     'catalog',
+    'checkout',
     
 ]
 
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'checkout.middleware.cart_item_middleware',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
